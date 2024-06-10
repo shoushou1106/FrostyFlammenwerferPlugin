ed23215de319a0b61d718eeaf01efc2fecb759c9
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-06-10 21:44:35 +0800

    BREAKING CHANGE: add editor project

---
+++ FlammenwerferPlugin.Editor/Extensions/LocalizedStringEditorMenuExtension.cs
@@ -0,0 +1,22 @@
+﻿using FlammenwerferPlugin.Editor.Controls;
+using Frosty.Core;
+using System.Windows.Media;
+
+namespace FlammenwerferPlugin.Editor.Extensions
+{
+    public class LocalizedStringViewerMenuExtension : MenuExtension
+    {
+        internal static ImageSource imageSource = new ImageSourceConverter().ConvertFromString("pack://application:,,,/FlammenwerferPlugin.Editor;component/Images/LocalizedStringEditor.png") as ImageSource;
+
+        public override string TopLevelMenuName => "View";
+
+        public override string MenuItemName => "Localized String Editor";
+
+        public override ImageSource Icon => imageSource;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            App.EditorWindow.OpenEditor("Localized String Editor", new LocalizedStringEditor(App.Logger));
+        });
+    }
+}
