246f4c6c167a314509f6eff9a0ba5615cf509d71
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-06-11 22:30:23 +0800

    feat: import window

---
+++ FlammenwerferPlugin.Editor/Windows/ImportStringWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Frosty.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlammenwerferPlugin.Editor.Windows
+{
+    public partial class ImportStringWindow : FrostyDockableWindow
+    {
+        public ImportStringWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
